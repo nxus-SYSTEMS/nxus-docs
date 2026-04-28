ca6fc44b65e318eecb003eb4c37a3592e6f37ca2
Ken <ken.dellinger@nxussystems.onmicrosoft.com>
2026-04-28 17:18:58 -0500

    Add structured data to docs pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -49,6 +49,7 @@ export default defineConfig({
 			],
 			customCss: ['./src/styles/custom.css'],
 			components: {
+				Head: './src/components/Head.astro',
 				Header: './src/components/Header.astro',
 				MobileMenuFooter: './src/components/MobileMenuFooter.astro',
 				ThemeProvider: './src/components/ThemeProvider.astro',
